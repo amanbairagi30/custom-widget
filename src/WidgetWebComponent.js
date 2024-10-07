8bdd9e132dc074deff73b0afee861a9b599010cf
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-10-07 22:53:31 +0530

    fix

--- src/WidgetWebComponent.js
+++ src/WidgetWebComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import "./index.css"
 import Widget from './Widget.jsx';
 
@@ -17,7 +17,7 @@ class WidgetWebComponent extends HTMLElement {
     const theme = this.getAttribute('theme') || 'light';
     const username = this.getAttribute('username') || 'default-user';
 
-    ReactDOM.createRoot(
+    ReactDOM.render(
       <React.StrictMode>
         <Widget theme={theme} username={username} />
       </React.StrictMode>,
